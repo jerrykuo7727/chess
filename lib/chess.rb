349bc8259cb3102efff007fdbee5379ccf30f901
jerrykuo7727 <jerrykuo7727@gmail.com>
2016-08-09 10:07:50 +0800

    Build chess#valid_input?

--- lib/chess.rb
+++ lib/chess.rb
@@ -23,7 +23,18 @@ class Chess
     end
     puts '   ａ ｂ ｃ ｄ ｅ ｆ ｇ ｈ '
   end
+
+  #def ask_piece
+  #
+  #  puts 'Which piece to move: '
+
+  def valid_input?(input)
+    return false if input.length != 2
+    return false unless 'ABCDEFGHabcdefgh'.include?(input[0])
+    return false unless '12345678'.include?(input[1])
+    return true
+  end
 end
 
 chess = Chess.new
-chess.display
+p chess.valid_input?('A0')
