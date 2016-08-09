fe8a2fb1d26de1842b71ace24d3bd462d02f34be
jerrykuo7727 <jerrykuo7727@gmail.com>
2016-08-09 13:43:15 +0800

    Add #valid_move_for_bishop? rspec test

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -38,6 +38,24 @@ describe Chess do
       expect(chess.send(:valid_move_for_queen?, [3,3], [6,0])).to eql(false)
       expect(chess.send(:valid_move_for_queen?, [3,3], [1,2])).to eql(false)
       expect(chess.send(:valid_move_for_queen?, [3,3], [0,6])).to eql(false)
-    end 
+    end
+  end
+
+  describe '#valid_move_for_bishop?' do
+    it 'returns true when clear to move' do
+      chess.instance_variable_set(:@board,
+              [['.', '.', '.', '.', '.', '.', '♘', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '♘', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '♗', '.', '.', '.', '.'],
+               ['.', '.', '♘', '.', '.', '.', '.', '.'],
+               ['.', '.', '♘', '.', '.', '♞', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.'],
+               ['.', '.', '.', '.', '.', '.', '.', '.']])
+      expect(chess.send(:valid_move_for_bishop?, [3,3], [0,0])).to eql(true)
+      expect(chess.send(:valid_move_for_bishop?, [3,3], [5,5])).to eql(true)
+      expect(chess.send(:valid_move_for_bishop?, [3,3], [6,0])).to eql(false)
+      expect(chess.send(:valid_move_for_bishop?, [3,3], [0,6])).to eql(false)
+    end
   end
 end
